93278111f7daa435bf4857bf82ffd8fa868603f0
Delila Gama <delilagama@gmail.com>
2024-01-16 18:12:33 -0800

    Added optimized solution for ContainsDuplicate Neetcode solution.

--- 217-containsduplicate.py
+++ 217-containsduplicate.py
@@ -4,7 +4,7 @@ and return false if every element is distinct.
 """
 
 class Solution:
-    def containsDuplicate(self, nums: list[int]) -> bool:
+    def containsDuplicateBrute(self, nums: list[int]) -> bool:
         for i in range(len(nums)):
             for j in range(len(nums)):
                 if i != j and nums[i] == nums[j]:
@@ -12,11 +12,19 @@ class Solution:
         # If we reach here, there are no duplicates.
         return False
 
-
+    def containsDuplicateOptimized(self, nums: list[int]) -> bool:
+        s = set()
+        for i in range(len(nums)):
+            if nums[i] in s:
+                return True
+            else:
+                s.add(nums[i])
+        return False
+            
 a = [1,2,3]
 test1 = Solution()
-print(test1.containsDuplicate(a)) # Should return False
+print(test1.containsDuplicateOptimized(a)) # Should return False
 
 b = [1,2,3,1]
 test2 = Solution()
-print(test2.containsDuplicate(b)) # Should return True
+print(test2.containsDuplicateOptimized(b)) # Should return True
